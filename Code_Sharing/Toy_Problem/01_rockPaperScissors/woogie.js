6e1f94a25d70224e4d03e68119ef7a0fa40b6f69
Woogie-94 <keu1106@naver.com>
2021-02-28 21:05:55 +0900

    done rockpaperscissors woogie

--- Code_Sharing/Toy_Problem/01_rockPaperScissors/woogie.js
+++ Code_Sharing/Toy_Problem/01_rockPaperScissors/woogie.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const rockPaperScissors = function (n) {
+  n = n || 3;
+  let arr = ["rock", "paper", "scissors"];
+
+  function permutation(arr, selectNum) {
+    const result = [];
+    if (selectNum === 1) return arr.map((v) => [v]);
+
+    arr.forEach((v, idx, arr) => {
+      const fixed = v;
+      const restArr = arr;
+      const permutationArr = permutation(restArr, selectNum - 1);
+      const combineFix = permutationArr.map((v) => [fixed, ...v]);
+      result.push(...combineFix);
+    });
+    return result;
+  }
+
+  return permutation(arr, n);
+};
