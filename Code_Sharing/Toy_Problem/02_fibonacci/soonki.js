55de1f38e6419bfa0b861e846becf992cce1ced5
SoonKi Kwon <jhk327@gmail.com>
2021-02-28 20:31:20 +0900

    Add Toy_02 by soonki

--- Code_Sharing/Toy_Problem/02_fibonacci/soonki.js
+++ Code_Sharing/Toy_Problem/02_fibonacci/soonki.js
@@ -1,1 +1,20 @@
-어제 자기전에 생각난 방법으로 푸는중
+function fibonacci(n) {
+  // TODO: 여기에 코드를 작성합니다.
+  let arr = [0, 1];
+
+  function inputEl(num) {
+    num = arr.length;
+
+    if (num - 1 === n) return;
+    else {
+      arr[num] = arr[num - 1] + arr[num - 2];
+      return inputEl();
+    }
+  }
+
+  if (n <= 2) {
+    return arr[n];
+  }
+  inputEl();
+  return arr[arr.length - 1];
+}
