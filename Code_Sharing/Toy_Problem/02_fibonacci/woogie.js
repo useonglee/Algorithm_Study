f8740949f067850ff1d08e6023ca1272293e7264
Woogie-94 <keu1106@naver.com>
2021-03-07 20:32:27 +0900

    done fibonacci

--- Code_Sharing/Toy_Problem/02_fibonacci/woogie.js
+++ Code_Sharing/Toy_Problem/02_fibonacci/woogie.js
@@ -1,1 +1,8 @@
-console.log("hi");
+function fibonacci(n, fib = [0, 1]) {
+  if (n === 0) return 0;
+
+  fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  fibonacci(n - 1, fib);
+
+  return fib[n];
+}
