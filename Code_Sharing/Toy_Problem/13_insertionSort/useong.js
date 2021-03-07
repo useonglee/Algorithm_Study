43d921aa97d87fad2c8548ee42234361da29470d
useonglee <useong0830@naver.com>
2021-03-07 23:39:12 +0900

    updated to Toy 13 by useong

---
+++ Code_Sharing/Toy_Problem/13_insertionSort/useong.js
@@ -0,0 +1,25 @@
+const insertionSort = (arr, result = (el) => el) => { 
+  
+  for (let i = 0; i < arr.length-1; i++) {
+    let swap = 0;
+    let isSwap = true;
+
+    for (let j = 0; j < arr.length-1-i; j++) {
+      swap = arr[j];
+
+      if (result(arr[j+1]) < result(arr[j])) {
+
+        arr[j] = arr[j+1]
+        arr[j+1] = swap;
+        isSwap = false;
+      }
+    }
+
+    if (isSwap) {
+      break;
+    }
+  }
+  return arr;
+}
+  
+
