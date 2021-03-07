4beb83a79904b9e014284362d882c4a36322438d
Woogie-94 <keu1106@naver.com>
2021-03-07 20:32:44 +0900

    done isSubsetOf

--- Code_Sharing/Toy_Problem/03_isSubsetOf/woogie.js
+++ Code_Sharing/Toy_Problem/03_isSubsetOf/woogie.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const isSubsetOf = function (base, sample) {
+  let answer = false;
+  const baseSet = new Set(base);
+
+  for (let i = 0; i < sample.length; i++) {
+    if (baseSet.has(sample[i])) answer = true;
+    else answer = false;
+  }
+
+  return answer;
+};
