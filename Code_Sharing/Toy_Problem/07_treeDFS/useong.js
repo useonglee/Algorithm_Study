2ef4ea125c67697678c51025d5301f44b77cd02d
useonglee <useong0830@naver.com>
2021-03-23 09:09:08 +0900

    updated by useonglee

--- Code_Sharing/Toy_Problem/07_treeDFS/useong.js
+++ Code_Sharing/Toy_Problem/07_treeDFS/useong.js
@@ -1,3 +1,20 @@
-const treeDFS = () => {
-  return "곧 풀 예정"
-}
+let dfs = function (node) {
+
+  let stack = [node.value];
+  for (let i = 0; i < node.children.length; i++) {
+    stack.push(...dfs(node.children[i]))
+  }
+  return stack;
+};
+
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
+
+
